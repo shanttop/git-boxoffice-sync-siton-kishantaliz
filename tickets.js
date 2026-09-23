c84e1c147ef7c9eebd503c6bd8e422d5d48d6e51
shanttop <niamaorhu@gmail.com>
2026-09-23 12:00:57 +0800

    Add 10% group discount for orders of 5+ - siton.kishantaliz

--- tickets.js
+++ tickets.js
@@ -3,7 +3,11 @@ function isValidQuantity(quantity) {
 }
 
 function calculateTicketPrice(quantity, basePrice) {
-  return Math.floor(quantity * basePrice);
+  let total = quantity * basePrice;
+  if(quantity >= 5) {
+    total = total * 0.9; // Apply a 10% discount for 5 or more tickets
+  }
+  return Math.floor(total);
 }
 
 module.exports = { isValidQuantity, calculateTicketPrice };
